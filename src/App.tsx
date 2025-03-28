757d1f6686693b25eb03cb6685f297bafceb58bf
Allwin P G <allwin.george19@gmail.com>
2025-03-28 19:30:12 +0530

    generic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Box } from "./component/Context/Box"
 import { ThemecontextProvider } from "./component/Context/Themecontext"
+import { User } from "./component/Context/UserContext/User"
+import { UserContextProvider } from "./component/Context/UserContext/UserContext"
+import { List } from "./component/Generics/List"
 import { Counter } from "./component/Reducer/Counter"
 import { IsLogged } from "./component/States/IsLogged"
 import { UserLogin } from "./component/States/UserLogin"
@@ -44,6 +47,30 @@ const nameList = [
     <ThemecontextProvider>
       <Box />
     </ThemecontextProvider>
+
+    <UserContextProvider>
+      <User />
+    </UserContextProvider>
+
+    <List items ={[1,2,3]} onClick={(item) => console.log(item)}/>
+    <List items = {["spiderman", "wonder women", "super man"]} onClick={(item) => console.log(item)} />
+    <List items = {[ 
+      {
+        id: 1,
+        first: "Bruce",
+        last: "Wayne"
+      },
+      {
+        id:2,
+        first: "Clark",
+        last: "Kent"
+      },
+      {
+        id:3,
+        first: "princess",
+        last: "Diana"
+      }
+    ]} onClick={(item)=> console.log(item)}/>
   </div>
   )
 }
